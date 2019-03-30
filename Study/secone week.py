f3571d0671d0f809916a2d6864cf54340010a4a0
17310 <17310958353@163.com>
2019-03-31 01:12:51 +0800

    int

--- Study/secone week.py
+++ Study/secone week.py
@@ -1,5 +1,54 @@
 s = "淘金者"
 print(s.encode("utf-8"))
 
+#用什么编码方式进行编码，就用什么编码方式进行解码
+
 bs = b'\xe6\xb7\x98\xe9\x87\x91\xe8\x80\x85'
 print(bs.decode("utf-8"))
+
+
+#字符串
+s = "周的杰阿里里伦巴巴的呵呵"  #  把周杰伦 拼接成一个新的字符串并打印
+print(s[0],s[2],s[6])
+s = ("%s%s%s"%(s[0],s[2],s[6]))
+print(type(s),s)
+
+#字符串切片
+#语法：字符串[start;end:步长] 顾头不顾尾   步长控制切片的方向，正数从左到右，负数从右到左
+s = "Java,Python,Php,C++"
+print(s[5:11])
+
+s1 = "123456789"
+print(s1[::2])
+print(s1[-2::-2])
+information = "aNxYV"
+infor = input("请输入验证码%s"%information)
+if infor.upper() == information.upper():
+    print("登陆成功")
+else:
+    print("验证码错误")
+
+
+#for 循环
+s = "dgfjkwej"
+for i in s:
+    print(i)
+
+#列表
+
+list = ["zhangsan","lisi","wangwu"]
+list.append("wangermazi")   #再列表后追加元素
+list.insert(0,"sunliu")     #在列表中插入一个元素，原有位置的元素向后推一位
+list.extend(["张三","李四"])    #合并列表
+#删除
+list.pop(2)  #删除指定位置的元素
+list.remove("李四")   #删除与之匹配的元素
+list.clear()    #清空列表
+print(list)
+
+infor = input("请输入一个加法运算：")
+infor = infor.split("+")
+count = 0
+for i in infor:
+     count += int(i)
+print(count)

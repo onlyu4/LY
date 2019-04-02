02b6e67a52dbc74ccfd83880036750635ecf2045
17310 <17310958353@163.com>
2019-04-02 17:41:11 +0800

    增加了发送邮件

---
+++ monitor/发送邮件.py
@@ -0,0 +1,26 @@
+
+# -*- coding: UTF-8 -*-
+#Author:liuyan
+
+import smtplib
+from email.mime.text import MIMEText
+
+msg_from = '17310958353@163.com'  # 发送方邮箱
+passwd = 'Loveme1314'  # 填入发送方邮箱的授权码
+msg_to = 'liuyan1@niuguwang.com'  # 收件人邮箱
+
+subject = "python邮件测试"  # 主题
+content = "这是我使用python smtplib及email模块发送的邮件"   # 正文
+msg = MIMEText(content)
+msg['Subject'] = subject
+msg['From'] = msg_from
+msg['To'] = msg_to
+try:
+    s = smtplib.SMTP_SSL("smtp.163.com", 465)    # 邮件服务器及端口号
+    s.login(msg_from, passwd)
+    s.sendmail(msg_from, msg_to, msg.as_string())
+    print("发送成功")
+except Exception as err:
+    print("发送失败")
+finally:
+    s.quit()

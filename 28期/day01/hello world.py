86c75a6cb804b0e247cf5c510ca60d40c1b8eea3
17310 <17310958353@163.com>
2019-11-02 09:33:11 +0800

    新增28期

---
+++ 28期/day01/hello world.py
@@ -0,0 +1,13 @@
+#
+#
+# print("hello world")
+#
+# print("""文能提笔安天下，
+# 武能马上定乾坤。
+# 心存谋略何人胜，
+# 古今英雄唯是君。""")
+
+salary = input("请输入你得工资")
+tax = int(salary) * 0.02
+salary_exit = int(salary) - int(tax)
+print(salary_exit)
